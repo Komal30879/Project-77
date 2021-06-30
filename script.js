a4b6d504664be5a33528a2e7c186c0966003c83e
Komal30879 <82501759+Komal30879@users.noreply.github.com>
2021-06-30 10:58:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var first_name = "Komal";
+console.log(first_name);
+
+var last_name = " Tripathy";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = "10";
+var b = "5";
+
+var add = a + b ;
+console.log(add);
+
+var subtarct = a-b;
+console.log(subtarct);
+
+var multiply = a*b;
+console.log(multiply);
+
+var divide = a/b;
+console.log(divide);
+
+var maximumvalue = Math.max(a,b);
+console.log(maximumvalue);
+
+var minimumvalue = Math.min(a,b);
+console.log(minimumvalue);
